64b3b26d4a3a7839f84f55a506634aa19e81d5ca
Ravdrew <andresegarcia@me.com>
2022-10-28 13:12:17 -0700

    differentiating speed

--- include/flywheel.h
+++ include/flywheel.h
@@ -3,10 +3,13 @@
 #ifndef FLY_H_
 #define FLY_H_
 
+extern double f_target;
+
 int sgn(double input);
 //void changeTBHTarget(float speed);
 //void takeBackHalf();
 void flywheelMove(double speed);
 void flywheelBrake();
+void flywheelPDF();
 
 #endif
